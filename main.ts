090b13941cb6f097c497a3b841f407ba3a1f0d39
Viktor Nagy <viktor.nagy@365.bank>
2023-10-20 15:51:46 +0200

    init: decode string

---
+++ main.ts
@@ -0,0 +1,31 @@
+type PossibleValues = string | number;
+type Data = PossibleValues | Record<string, PossibleValues> | PossibleValues[];
+
+const bencode_decode = (str: string): Data => {
+  if (str[0] == "i") {
+    return 0;
+  } else if (str[0] == "l") {
+    return 0;
+  } else if (str[0] == "d") {
+    return 0;
+  } else {
+    const charCount = Number(str[0]);
+    const dataPart = str.split(":");
+    const data = dataPart[1].slice(0, charCount);
+    return data;
+  }
+};
+
+function main() {
+  // const args = process.argv.slice(2);
+  const toProcess = "5:hello";
+  const processed = bencode_decode(toProcess);
+  console.log(
+    "================\n",
+    "processed: ",
+    processed,
+    "\n================",
+  );
+}
+
+main();
