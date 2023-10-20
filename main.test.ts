98ebe8cc17e7c8a0ee67c31ed1c44c5cf6cafd20
Viktor Nagy <viktor.nagy@365.bank>
2023-10-20 17:54:27 +0200

    feat: dict

--- main.test.ts
+++ main.test.ts
@@ -13,3 +13,32 @@ test("bencode_decode int", () => {
   const result2 = bencode_decode("i-32e");
   expect(result2).toBe(-32);
 });
+
+test("bencode_decode list", () => {
+  const result = bencode_decode("li32e3:lole");
+  expect(result).toMatchObject([32, "lol"]);
+});
+
+test("bencode_decode dict", () => {
+  const result = bencode_decode("d3:loli32ee");
+  expect(result).toMatchObject({
+    lol: 32,
+  });
+
+  const result2 = bencode_decode("d3:loli32e4:hahal4:lole3:yesee");
+  expect(result2).toMatchObject({
+    lol: 32,
+    haha: ["lole", "yes"],
+  });
+
+  const result3 = bencode_decode(
+    "d4:nestd1:ai33ee3:loli32e4:hahal3:lol3:yesee",
+  );
+  expect(result3).toMatchObject({
+    nest: {
+      a: 33,
+    },
+    lol: 32,
+    haha: ["lol", "yes"],
+  });
+});
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-type PossibleValues = string | number;
-type Data = PossibleValues | Record<string, PossibleValues> | PossibleValues[];
+// @ts-ignore
+type Data = number | string | Record<string, Data> | Data[];
 
 export const bencode_decode = (str: string): Data => {
   const [data, remainder] = bencode_decode_recurse(str);
@@ -12,9 +12,26 @@ const bencode_decode_recurse = (str: string): [Data, string] => {
     if (ePosition === -1) throw Error("missing number ending");
     return [Number(str.slice(1, ePosition)), str.slice(ePosition + 1)];
   } else if (str[0] == "l") {
-    return [0, ""];
+    let innerListStr = str.slice(1);
+    const values: Data = [];
+    while (innerListStr[0] != "e") {
+      const [data, remainder] = bencode_decode_recurse(innerListStr);
+      values.push(data);
+      innerListStr = remainder;
+    }
+    return [values, innerListStr.slice(1)];
   } else if (str[0] == "d") {
-    return [0, ""];
+    let innerDictStr = str.slice(1);
+    const dict: Data = {};
+    while (innerDictStr[0] != "e") {
+      const [key, remainder] = bencode_decode_recurse(innerDictStr);
+      const [value, remainder2] = bencode_decode_recurse(remainder);
+
+      if (typeof key !== "string") throw Error("key is not a string");
+      dict[key] = value;
+      innerDictStr = remainder2;
+    }
+    return [dict, innerDictStr.slice(1)];
   } else {
     const charCount = Number(str[0]);
     const dataPart = str.split(":");
@@ -25,14 +42,8 @@ const bencode_decode_recurse = (str: string): [Data, string] => {
 
 function main() {
   // const args = process.argv.slice(2);
-  const toProcess = "i32e";
+  const toProcess = "d4:nestd1:ai33ee3:loli32e4:hahal3:lol3:yesee";
   const processed = bencode_decode(toProcess);
-  console.log(
-    "================\n",
-    "processed: ",
-    processed,
-    "\n================",
-  );
 }
 
 main();
